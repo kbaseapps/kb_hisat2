1aa9fdc0f212d4082572a4e3fe0f1197615ca17c
Jay R Bolton <jayrbolton@gmail.com>
2020-06-15 18:08:52 +0000

    Fix one error

--- lib/kb_hisat2/hisat2.py
+++ lib/kb_hisat2/hisat2.py
@@ -368,7 +368,8 @@ class Hisat2(object):
         qm = kb_QualiMap(self.callback_url, service_ver='dev')
         qc_ref = alignment_set
         if qc_ref is None:  # then there's only one alignment...
-            qc_ref = alignments[alignments.keys()[0]]["ref"]
+            keys = list(alignments.keys())
+            qc_ref = alignments[keys[0]]["ref"]
         bamqc_params = {
             "create_report": 0,
             "input_ref": qc_ref
